bc5f44ae9d8f79f13f9d77ba22fce5cbff0e35bf
Lance Swan <swanlm@me.com>
2021-04-17 11:11:57 +0200

    Updated machine name and SanityCheck

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Wiggin-5"
+#define CUSTOM_MACHINE_NAME "Mazer-5"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
